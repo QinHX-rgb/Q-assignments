3ce1fe904e1639e110cafd7ed69a961ce0c4726b
QinHX-rgb <DinningCasgrove@gmail.com>
2026-03-19 21:18:56 +0800

    初始化LED

---
+++ ex03/sketch_mar19a/sketch_mar19a.ino
@@ -0,0 +1,13 @@
+#define LED_PIN_R 13
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_PIN_R, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
