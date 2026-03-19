c30921c8c8b3735880324cb14f39d792d0b8ba2f
QinHX-rgb <DinningCasgrove@gmail.com>
2026-03-19 21:07:25 +0800

    初始化外接LED

--- ex02/sketch_mar19a/sketch_mar19a.ino
+++ ex02/sketch_mar19a/sketch_mar19a.ino
@@ -1,11 +1,6 @@
-unsigned long myTime;
+#define LED_PIN_R 13
 
 void setup() {
   Serial.begin(115200);
-}
-
-void loop() {
-  myTime = millis(); // 获取当前时间
-  Serial.println(myTime); // 在串口监视器打印，你会看到数字飞速增长
-  delay(1000);
+  pinMode(LED_PIN_R, OUTPUT);
 }
