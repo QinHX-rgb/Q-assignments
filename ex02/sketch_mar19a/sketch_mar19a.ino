66546f37016c60acf9968cc41be23887b8a2d68c
QinHX-rgb <DinningCasgrove@gmail.com>
2026-03-19 21:13:09 +0800

    使用millis函数实现LED闪烁

--- ex02/sketch_mar19a/sketch_mar19a.ino
+++ ex02/sketch_mar19a/sketch_mar19a.ino
@@ -1,6 +1,16 @@
 #define LED_PIN_R 13
+unsigned long previousMillis = 0; // 记录上一次变灯的时间
+int state = LOW;          // 记录灯是亮还是灭
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN_R, OUTPUT);
+}
+
+void loop() {
+  if (millis() - previousMillis >= 500) {
+    previousMillis = millis();
+    state = !state;
+    digitalWrite(LED_PIN_R, state);
+  }
 }
