ef97e8fdcd64b5bbdac4a00ba7ca05a64a5081d2
QinHX-rgb <DinningCasgrove@gmail.com>
2026-03-26 20:45:15 +0800

    引入millis函数

--- ex06/sketch_mar26b/sketch_mar26b.ino
+++ ex06/sketch_mar26b/sketch_mar26b.ino
@@ -10,6 +10,8 @@ int brightness = 0;
 int fadeAmount = 5;
 int interval = 10;//频闪速率
 
+unsigned long lastFadeTime = 0;
+
 void setup() {
    Serial.begin(115200);
    
@@ -18,5 +20,10 @@ void setup() {
 }
 
 void loop() {
- 
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - lastFadeTime >= interval) {
+    lastFadeTime = currentMillis;
+
+  
 }
