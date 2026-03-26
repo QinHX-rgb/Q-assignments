3b61e2ac4773d3b6f0e499b1b8bb460d1eb2282f
QinHX-rgb <DinningCasgrove@gmail.com>
2026-03-26 20:12:08 +0800

    在ex04的基础上引入PWM设置参数

---
+++ ex05/sketch_mar26a/sketch_mar26a.ino
@@ -0,0 +1,36 @@
+#define LED_PIN 2
+#define TOUCH_PIN 13
+#define THRESHOLD 600
+
+// PWM 设置
+const int freq = 5000;
+const int resolution = 8;
+
+int speedLevel = 1; 
+unsigned long lastTriggerTime = 0;
+
+void  gotTouch() {
+  unsigned long currentTime = millis();
+  
+  if (currentTime - lastTriggerTime > 300) {
+    ledState = !ledState;
+    digitalWrite(LED_PIN, ledState);
+    lastTriggerTime = currentTime;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  
+  touchAttachInterrupt(TOUCH_PIN, gotTouch, THRESHOLD);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Touch Value: ");
+  Serial.println(touchRead(TOUCH_PIN));
+  delay(100);
+}
