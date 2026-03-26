bdd09b1bca669c61d35123867a32d43688b249c8
QinHX-rgb <DinningCasgrove@gmail.com>
2026-03-26 20:45:59 +0800

    呼吸

--- ex06/sketch_mar26b/sketch_mar26b.ino
+++ ex06/sketch_mar26b/sketch_mar26b.ino
@@ -12,18 +12,19 @@ int interval = 10;//频闪速率
 
 unsigned long lastFadeTime = 0;
 
-void setup() {
-   Serial.begin(115200);
-   
-  ledcAttach(LED_PIN_R, freq, resolution);
-  ledcAttach(LED_PIN_B, freq, resolution);
-}
-
 void loop() {
   unsigned long currentMillis = millis();
-  
+
   if (currentMillis - lastFadeTime >= interval) {
     lastFadeTime = currentMillis;
 
-  
+    ledcWrite(LED_PIN_R, brightness);
+    ledcWrite(LED_PIN_B, 255 - brightness);
+
+    brightness += fadeAmount;
+
+    if (brightness <= 0 || brightness >= 255) {
+      fadeAmount = -fadeAmount;
+    }
+  }
 }
