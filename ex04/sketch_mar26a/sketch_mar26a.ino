886d37a0744350abfbf1f9d950ac21ed0bdf4e88
QinHX-rgb <DinningCasgrove@gmail.com>
2026-03-26 19:23:51 +0800

    定义布尔型状态变量，引入millis函数防抖

--- ex04/sketch_mar26a/sketch_mar26a.ino
+++ ex04/sketch_mar26a/sketch_mar26a.ino
@@ -2,6 +2,19 @@
 #define TOUCH_PIN 13
 #define THRESHOLD 600
 
+bool ledState = false;
+unsigned long lastTriggerTime = 0;
+
+void  gotTouch() {
+  unsigned long currentTime = millis();
+  
+  if (currentTime - lastTriggerTime > 300) {
+    ledState = !ledState;
+    digitalWrite(LED_PIN, ledState);
+    lastTriggerTime = currentTime;
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
